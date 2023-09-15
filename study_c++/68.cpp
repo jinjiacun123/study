1db38d066e9fe9d2d3d4515bdd1e813c0cc10395
jinjiacun123 <jinjiacuncucu@sina.com>
2023-09-15 20:37:52 +0800

    finish 68

--- study_c++/68.cpp
+++ study_c++/68.cpp
@@ -13,10 +13,12 @@ public:
       int curPosition = 0;
       int maxSpitLength;
       int spaceLength;
+      int isLastGroup = 0;
 
       while(curPosition < wordsLen){
 	maxSpitLength = 0;
 	spaceLength = 0;
+	isLastGroup = 0;
       
 	beginPosition = curPosition;
 	while(curPosition + maxSpitLength < wordsLen
@@ -24,18 +26,74 @@ public:
 	  spaceLength += words[curPosition+maxSpitLength].size() + 1;
 	  maxSpitLength++;	  
 	}
+	
 	//save string group
 	if(maxSpitLength > 0){
+	  //only one element
+	  if(maxSpitLength == 1){
+	    string str = words[curPosition+maxSpitLength -1];
+	    str.append(maxWidth - str.size() , ' ');
+	    retVector.push_back(str);
+	    // cout<<"---->maxSpitLenth:1,str:["<<str<<"]"<<endl;
+	    curPosition += maxSpitLength;	    
+	    continue;
+	  }
+	  //cout<<"#####maxSpitLength:"<<maxSpitLength<<endl;
 	  //is over
-	  if(spaceLength > maxWidth){
-	    maxSpitLength--;	    
+	  if(spaceLength > maxWidth + 1){
+	    spaceLength -= words[curPosition+maxSpitLength -1].size() + 1;
+	    maxSpitLength--;	   
+	  }
+	  spaceLength --;//clean end black
+	  //cout<<"########-->:"<< wordsLen - curPosition - maxSpitLength<<endl;
+	  if(wordsLen - curPosition - maxSpitLength == 0){isLastGroup = 1;}
+	  int restBlackNum = maxWidth - spaceLength;
+	  if(restBlackNum > 0){
+	    //need fill with black
+	    /*
+	    cout<<"need fill:"<<restBlackNum
+		<<",spaceLength:"<<spaceLength
+	        <<",maxSpitLength:"<<maxSpitLength
+	        <<",curPosition:"<<curPosition
+		<<endl;
+	    */
 	  }
 	  string str;
-	  for(int i = 0; i < maxSpitLength - 1; i++){
-	    str += words[curPosition + i] + " ";
+	  vector<int> fillLenVector = generalAnBySn(restBlackNum, maxSpitLength-1);
+	  //cout<<"sequence:";
+	  /*
+	  for(int i = 0; i < fillLenVector.size(); i ++){
+	    cout<<fillLenVector[i]<<",";
 	  }
-	  str += words[curPosition + maxSpitLength -1];
-	  retVector.push_back(str);
+	  cout<<endl;
+	  */
+	  if(maxSpitLength > 1){
+	    //int isLastGroup = 0;
+	    //check is last group
+	    cout<<"retVector count:"<<retVector.size()<<endl;
+	    for(int i = 0; i < maxSpitLength - 1; i++){
+	      string tmpStr =  words[curPosition + i];
+	      if(isLastGroup == 0){
+		int tmpLen = fillLenVector[i] + 1;
+		//= (restBlackNum/(maxSpitLength -1)) + 1;
+		tmpStr.append(tmpLen, ' ');
+	      }else if(isLastGroup == 1){
+		  tmpStr.append(1, ' ');
+	      }
+	      str += tmpStr;
+	    }
+	    if(isLastGroup == 0)
+	      str += words[curPosition + maxSpitLength -1];
+	    else{
+	       str += words[curPosition + maxSpitLength -1];
+	       str.append(maxWidth - str.size(), ' ');
+	    }
+	  }else if(maxSpitLength == 1){
+	    string tmpStr =  words[curPosition + 0];
+	    tmpStr.append(maxWidth - tmpStr.size(), ' ');
+	    str += tmpStr;
+	  }
+	   retVector.push_back(str);
 	}
 
 	curPosition += maxSpitLength;
@@ -46,6 +104,29 @@ public:
       
       return retVector;
     }
+
+  //general spit black sequence
+  vector<int> generalAnBySn(int sn, int n){//black of left may great or equal black of right 
+    vector<int> sequence;
+    
+    if(n == 0){
+      sequence.push_back(sn);
+      return sequence;
+    }
+    
+    for(int i = 0; i < n; i ++){
+      sequence.push_back(sn/n);
+    }
+
+    int rest = sn - (sn/n)*n;
+    if(rest > 0){
+      for(int i = 0; i < n,rest > 0; i++,rest--){
+	sequence[i] += 1;
+      }
+    }
+
+    return sequence;
+  }
 };
 
 void
@@ -114,17 +195,19 @@ test(Solution s,
   
  
   for(auto [k,v]:paramA){
+    cout<<"lab "<<k<<":"<<endl;
     vector<string> value = s.fullJustify(v, paramB[k]);
     if(value.size() <= 0){      
       cout<<k<<"-->fail"<<endl;
       continue;
     }
-    cout<<"lab "<<k<<":"<<endl;
+
     for(int i = 0; i < value.size(); i++){
       cout<<"i:"<<i<<",value:["<<value[i]<<"]"<<endl;
     }
     cout<<"--------------------"<<endl;
-    continue;
+    //    break;
+    //continue;
     int resultLen = result[k].size();
     int okTime = 0;
     for(int i = 0; i < resultLen; i++){
@@ -156,7 +239,12 @@ main(){
 	map<int, int> b;
 	map<int, vector<string>> result;
 
-	test(s, a, b, result);
+		test(s, a, b, result);
+
+	/*string ss = "foo";
+	ss.append(20 - ss.length(), ' ');
+	cout<<"ss:["<<ss<<"]"<<endl;
+	*/
 
 	return 0;	
 }
